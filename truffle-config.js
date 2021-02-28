9fa28e502bd045fe681533c6a9aec64e474a08de
Tiranan <tiranan.e@ku.th>
2021-02-28 20:48:10 +0700

    prepare for deploy in vercel

---
+++ truffle-config.js
@@ -0,0 +1,32 @@
+require('babel-register');
+require('babel-polyfill');
+const { id, seed } = require('./env.json');
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(seed, "https://ropsten.infura.io/v3/"+ id)
+      },
+      network_id: 3,
+      skipDryRun: true
+    },
+  },
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+      evmVersion: "petersburg"
+    }
+  }
+}
